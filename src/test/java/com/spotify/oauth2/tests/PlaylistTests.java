0f19b046b21c0266d0532ae872ffbbf6e412aa47
ayborG <ayberk.ozturk22@gmail.com>
2026-04-24 18:14:34 +0300

    Create Config Loader Singleton Design Pattern

--- src/test/java/com/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/com/spotify/oauth2/tests/PlaylistTests.java
@@ -2,6 +2,7 @@ package com.spotify.oauth2.tests;
 import com.spotify.oauth2.api.applicationApi.PlaylistApi;
 import com.spotify.oauth2.pojo.Error;
 import com.spotify.oauth2.pojo.Playlist;
+import com.spotify.oauth2.utils.DataLoader;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,81 +10,65 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PlaylistTests {
 
+
     @Test
     public void ShouldBeAbleToCreatePlaylist() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("New Playlist")
-                .setDescription("New playlist description")
-                .setPublic(false);
-
+        Playlist requestPlaylist = playlistBuilder("New Playlist","New playlist description",false);
         Response response = PlaylistApi.post(requestPlaylist);
-        assertThat(response.statusCode(),equalTo(201));
-
+        assertStatusCode(response.statusCode(), 201);
         Playlist responsePlaylist = response.as(Playlist.class);
-
-        assertThat(responsePlaylist.getName(),equalTo(requestPlaylist.getName()));
-        assertThat(responsePlaylist.getDescription(),equalTo(requestPlaylist.getDescription()));
-        assertThat(responsePlaylist.getPublic(),equalTo(requestPlaylist.getPublic()));
-
+        assertPlaylistEqual(responsePlaylist,requestPlaylist);
     }
     @Test
     public void ShouldBeAbleToGetAPlaylist() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("Update Playlist Name")
-                .setDescription("Update playlist description")
-                .setPublic(true);
-        Response response = PlaylistApi.get("4F2I7431e2tKJ6CYAU3WEt");
-        assertThat(response.statusCode(),equalTo(200));
-
+        Playlist requestPlaylist = playlistBuilder("Update Playlist Name","Update playlist description",true);
+        Response response = PlaylistApi.get(DataLoader.getInstance().getPlaylistId());
+        assertStatusCode(response.statusCode(), 200);
         Playlist responsePlaylist = response.as(Playlist.class);
-
-        assertThat(responsePlaylist.getName(),equalTo(requestPlaylist.getName()));
-        assertThat(responsePlaylist.getDescription(),equalTo(requestPlaylist.getDescription()));
-        assertThat(responsePlaylist.getPublic(),equalTo(requestPlaylist.getPublic()));
+        assertPlaylistEqual(responsePlaylist,requestPlaylist);
 
     }
 
     @Test
     public void ShouldBeAbleToUpdateAPlaylist() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("Update Playlist Name")
-                .setDescription("Update playlist description")
-                .setPublic(false);
-
-        Response response = PlaylistApi.update("4F2I7431e2tKJ6CYAU3WEt",requestPlaylist);
-        assertThat(response.statusCode(),equalTo(200));
-
+        Playlist requestPlaylist = playlistBuilder("Update Playlist Name","Update playlist description",true);
+        Response response = PlaylistApi.update(DataLoader.getInstance().getPlaylistId(), requestPlaylist);
+        assertStatusCode(response.statusCode(), 200);
     }
     @Test
     public void ShouldNotBeAbleToCreatePlaylistWithName() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("")
-                .setDescription("New playlist description")
-                .setPublic(false);
+        Playlist requestPlaylist = playlistBuilder("","New playlist description",false);
         Response response = PlaylistApi.post(requestPlaylist);
-        assertThat(response.statusCode(),equalTo(400));
+        assertStatusCode(response.statusCode(),400);
         Error error = response.as(Error.class);
-
-        assertThat(error.getError().getStatus(),equalTo(400));
-        assertThat(error.getError().getMessage(),equalTo("Missing required field: name"));
-
+        assertError(error,400,"Missing required field: name");
     }
     @Test
     public void ShouldNotBeAbleToCreatePlaylistWithExpiredToken() {
         String invalid_token = "12312asdasd123123";
-        Playlist requestPlaylist = new Playlist()
-                .setName("New Playlist")
-                .setDescription("New playlist description")
-                .setPublic(false);
-
+        Playlist requestPlaylist = playlistBuilder("New Playlist","New playlist description",false);
         Response response = PlaylistApi.post(invalid_token,requestPlaylist);
-        assertThat(response.statusCode(),equalTo(401));
+        assertStatusCode(response.statusCode(),401);
         Error error = response.as(Error.class);
-
-
-        assertThat(error.getError().getStatus(),equalTo(401));
-        assertThat(error.getError().getMessage(),equalTo("Invalid access token"));
+        assertError(error,401,"Invalid access token");
+    }
+    public Playlist playlistBuilder(String name, String description, boolean _public){
+        return new Playlist()
+                .setName(name)
+                .setDescription(description)
+                .setPublic(_public);
+    }
+    public void assertPlaylistEqual(Playlist responsePlaylist,Playlist requestPlaylist){
+        assertThat(responsePlaylist.getName(),equalTo(requestPlaylist.getName()));
+        assertThat(responsePlaylist.getDescription(),equalTo(requestPlaylist.getDescription()));
+        assertThat(responsePlaylist.getPublic(),equalTo(requestPlaylist.getPublic()));
+    }
+    public void assertStatusCode(int actualStatusCode,int expectedStatusCode) {
+        assertThat(actualStatusCode,equalTo(expectedStatusCode));
 
     }
-
+    public void assertError(Error responseError, int expectedStatusCode,String expectedMsg) {
+        assertThat(responseError.getError().getStatus(),equalTo(expectedStatusCode));
+        assertThat(responseError.getError().getMessage(),equalTo(expectedMsg));
+    }
 }
